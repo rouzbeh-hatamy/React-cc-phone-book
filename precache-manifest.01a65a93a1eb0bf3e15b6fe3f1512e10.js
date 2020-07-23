131032e4d5eee5f807d3f7065eff22f467606568
Rouzbeh Hatamy <roozbeh.hatamy@gmail.com>
2020-07-23 12:16:40 +0430

    Updates

---
+++ precache-manifest.01a65a93a1eb0bf3e15b6fe3f1512e10.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "578d52fd52d53b5910b1d960dc533939",
+    "url": "/React-cc-phone-book/index.html"
+  },
+  {
+    "revision": "878e17cf036001a4ca0c",
+    "url": "/React-cc-phone-book/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "8c839ee44ea5d84e3fdb",
+    "url": "/React-cc-phone-book/static/css/main.86daf4d0.chunk.css"
+  },
+  {
+    "revision": "878e17cf036001a4ca0c",
+    "url": "/React-cc-phone-book/static/js/2.5391b6d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-cc-phone-book/static/js/2.5391b6d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c839ee44ea5d84e3fdb",
+    "url": "/React-cc-phone-book/static/js/main.44c44842.chunk.js"
+  },
+  {
+    "revision": "29a4357626445e4da11c",
+    "url": "/React-cc-phone-book/static/js/runtime-main.675b6e99.js"
+  }
+]);
